1d0c9a9b8f8bbfa3f0add4008efad455d5a66ebf
wungjyan <1170017412@qq.com>
2020-07-06 16:19:44 +0800

    完善公共设置组件

--- src/components/Charts/components/CommonSetting.vue
+++ src/components/Charts/components/CommonSetting.vue
@@ -22,14 +22,22 @@
     </p>
     <p>
       <el-switch v-model="showTitle" @change="changeTitleStatus" inactive-text="是否显示标题："></el-switch>
-      <p v-show="showTitle">
+      <div v-show="showTitle">
         <span class="setting-title">标题重命名：</span>
         <el-input v-model="chartTitle" @input="handleInputTitle" style="width:150px;" type="text" clearable size="mini" placeholder="默认是excel文件名"></el-input>
-      </p>
-      <p v-show="showTitle">
+      </div>
+      <div v-show="showTitle">
         <span class="setting-title">标题颜色：</span>
         <el-color-picker v-model="titleColor" @change="changeTitleColor" size="mini"></el-color-picker>
-      </p>
+      </div>
+    </p>
+    <p>
+      <el-switch v-model="legendVisible" @change="changeLegendStatus" inactive-text="是否显示图例："></el-switch>
+      <span class="after-tip">图例按钮可以点击的哦</span>
+    </p>
+    <p>
+      <el-switch v-model="tooltipVisible" @change="changeTooltipStatus" inactive-text="是否显示提示框："></el-switch>
+      <span class="after-tip">关闭时鼠标移入不显示提示框</span>
     </p>
     <p>
       <span class="setting-title">横坐标倾斜角度：</span>
@@ -48,27 +56,35 @@ export default {
       showTitle: false,
       chartTitle: '',
       titleColor: '#333',
-      rotate: 0
+      rotate: 0,
+      legendVisible: true,
+      tooltipVisible: true
     }
   },
   methods: {
     handleInputWidth (w) {
-      this.$emit('changeWidth', w)
+      this.$emit('change-width', w)
     },
     handleInputHeight (h) {
-      this.$emit('changeHeight', h)
+      this.$emit('change-height', h)
     },
     changeTitleStatus (s) {
-      this.$emit('changeTitleStatus', s)
+      this.$emit('change-title-status', s)
     },
     handleInputTitle (t) {
-      this.$emit('changeTitle', t)
+      this.$emit('change-title', t)
     },
     changeTitleColor (c) {
-      this.$emit('changeTitleColor', c)
+      this.$emit('change-title-color', c)
     },
     changeRotate (r) {
-      this.$emit('changeRotate', r)
+      this.$emit('change-rotate', r)
+    },
+    changeLegendStatus (l) {
+      this.$emit('change-legend-status', l)
+    },
+    changeTooltipStatus (t) {
+      this.$emit('change-tooltip-status', t)
     }
   }
 }
