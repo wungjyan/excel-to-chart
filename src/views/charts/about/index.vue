fc0b5f89b7344870b96ee2f16deed1a9e9a34591
wungjyan <1170017412@qq.com>
2020-08-28 11:21:56 +0800

    update

--- src/views/charts/about/index.vue
+++ src/views/charts/about/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>待完成...</div>
+  <div class="page-container">
+    <div>待完成...</div>
+    <div style="margin-top:20px;">可以先参考这里的文档部分：<a href="https://github.com/wungjyan/excel-to-chart" target="_blank">readme</a></div>
+  </div>
 </template>
 
 <script>
